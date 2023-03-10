890301231528c9aabfe22d29f9687fd24ddfe51e
Pietro Gallina <pietrogallina96@gmail.com>
2023-03-10 19:12:32 +0100

    Add @ResponseStatus(HttpStatus.NOT_FOUND) annotation to NotFoundException class, disabled error response's exception stack trace

--- src/main/java/co/develhope/mongoDb/exceptions/NotFoundException.java
+++ src/main/java/co/develhope/mongoDb/exceptions/NotFoundException.java
@@ -1,8 +1,13 @@
 package co.develhope.mongoDb.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
 
     public NotFoundException() {
         super("User not found");
     }
+
 }
